70e3c96257b8cc9f4f7b961447fce2bee94a7eee
烟雨江南 <cscms@qq.com>
2025-09-17 14:33:30 +0800

    修复无法在线更新升级

--- sys/libs/ver.php
+++ sys/libs/ver.php
@@ -6,7 +6,7 @@
  * @Dtime:2020-01-11
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
-define('Apiurl','Ly9hcGkuY2hzaGNtcy5uZXQvbWNjbXM');
+define('Apiurl','Ly9hcGkuY2hzaGNtcy5jb20vbWNjbXM');
 define('Zykurl','ec6ct0j2qX/FoAmtS6z2Z4ERee0hOtwaeVo4MfM6KBUL54SwZuHN1u1xdpIITY3ZS0KX4A');
 define('Zykbookurl','6766sIkDT6M9mvLMK5ZIJ6iWr0PDjXzUUZCmAzKWYXnJTe/3yXtnHwMz7Q1xKRRFA6Quyh78tag1');
 define('Ver','v2.7.0');
